e9f85b9890429b8474cf9a80cf54d7ebb20453dc
Gamez98 <gamez.rlg@hotmail.com>
2019-08-24 17:40:03 -0500

    change endpoint

--- index.py
+++ index.py
@@ -7,25 +7,25 @@ from flask import request
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
+@app.route("/train")
 def App():
-    with open('input_data_train.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            print(row[0])
-            out = "works" #json.dumps([row for row in reader])        
-        return out
+  with open('input_data_train.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+      print(row[0])
+      out = "works" #json.dumps([row for row in reader])        
+  return out
 
 
 @app.route("/upload", methods=['POST'])
 def Upload():
-    if request.method == 'POST':
-        print('got')
-        if 'file' not in request.files:
-            return "no files selected"
-        file = request.files['file']
-        print(file)
-        return "upload"
+  if request.method == 'POST':
+    print('got')
+    if 'file' not in request.files:
+      return "no files selected"
+    file = request.files['file']
+    print(file)
+    return "upload"
 
 
 
